88f16c7757efaa5c1757542bba92839daf322fdb
Alberto Gandoy Florido <alberto.gandoy@es-it03095.home>
2025-11-05 18:54:37 +0100

    Fix: i18n

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const links = [
     { to: '/', label: t('nav.home') },
